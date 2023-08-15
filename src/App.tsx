595cf38ebf35138d6cdf690862729711accf3fa8
Thales Galdino <thalesgaldino@gmail.com>
2023-08-15 20:57:41 +1200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,8 @@ import React from 'react';
 import SearchContainer from './SearchContainer';
 
 //TODO: fetch this from the build environment
-// uncomment with the constants
-// const API_KEY = '<ADD-KEY_HERE>';
-export const API_KEY = 'edf1b09ac2160046da111839bf4ba708';
+// Add key and set host
+export const API_KEY = '<ADD_KEY_HERE>';
 export const HOST = 'https://api.flickr.com';
 export const API_ENDPOINT = `${HOST}/services/rest`;
 
